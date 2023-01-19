9c2514944a4c4b81837da27fb4854cc6593f90d8
SaintMartyrn <dartagnanhickey@.com>
2023-01-18 18:52:09 -0800

    changed javascript

--- models/Post.js
+++ models/Post.js
@@ -1,61 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class Post extends Model { }
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            //has to be more than 10 characters
+            validate: {
+                len: [10]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4]
-      }
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
     }
-  },
-  {
-    hooks: {
-
-      async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-
-      async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      }
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
-  }
 );
 
-module.exports = User;
+module.exports = Post;
--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -2,17 +2,21 @@ async function deleteFormHandler(event) {
   event.preventDefault();
 
   const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+      window.location.toString().split('/').length - 1
   ];
+
   const response = await fetch(`/api/posts/${id}`, {
-    method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+          'Content-Type': 'application/json'
+      }
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard/');
+      document.location.replace('/dashboard/');
   } else {
-    alert(response.statusText);
+      alert(response.statusText);
   }
 }
 
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document.querySelector('#delete-post-btn').addEventListener('click', deleteFormHandler);
